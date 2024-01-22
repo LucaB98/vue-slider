29b4404f15d5e1a03bddda76a0aba3532cc972c1
LucaB98 <lucabellan98@gmail.com>
2024-01-22 18:37:26 +0100

    add figure and event on click

--- js/script.js
+++ js/script.js
@@ -3,13 +3,11 @@ console.log('vue ok', Vue);
 const {createApp} = Vue;
 
 const app = createApp({
-    data(){
-        return{
-            pictures,
-            currentIndex: 0
-        }
-    }
-});
+    data:() => ({
+        pictures,
+        currentIndex: 0
+    })
+})
 
 
 app.mount('#root');
