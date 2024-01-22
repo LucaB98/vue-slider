18827d810a696666cdf2619055e977ddf2ab5c3b
LucaB98 <lucabellan98@gmail.com>
2024-01-22 19:18:04 +0100

    add methods for currentIndex

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ const app = createApp({
         currentIndex: 0
     }),
     computed:{
+        lastElementIndex(){
+            return this.pictures.length - 1
+        },
         isLastIndex(){
             return this.currentIndex === this.pictures.length - 1;
         },
@@ -16,7 +19,20 @@ const app = createApp({
             return this.currentIndex === 0;
         }
     },
-    
+    methods:{
+      
+        setCurrentIndex(target){
+            if(target === 'next'){
+                if(this.isLastIndex) this.currentIndex = 0;
+                else this.currentIndex++;
+            }else if(target === 'prev'){
+                if(this.isFirstIndex)this.currentIndex = this.lastElementIndex;
+                else this.currentIndex--
+            }else{
+                this.currentIndex = target;
+            }
+        }
+    }
 })
 
 
